e3b1e8bc230c53c8a99dbb98b9cb16aac6ef70df
Alejogvy <Byui15.>
2025-06-01 18:51:25 -0500

    commit

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -146,6 +146,19 @@ public static class SetsAndMaps
         // on those classes so that the call to Deserialize above works properly.
         // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
         // 3. Return an array of these string descriptions.
-        return [];
+        if (featureCollection?.Features == null)
+            return [];
+        
+        var results = new List<string>();
+
+        foreach (var feature in featureCollection.Features)
+        {
+            var place = feature.Properties.Place ?? "Unknown location";
+            var mag = feature.Properties.Mag?.ToString("0.0") ?? "N/A";
+
+            results.Add($"Place: {place}, Magnitude: {mag}");
+        }
+
+        return results.ToArray();
     }
 }
